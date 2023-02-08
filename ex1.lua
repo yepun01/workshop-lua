1d274e4d4b75859767c5463b57b0d275151d7b93
Sofiane.Chardonnay <sofiane.chardonnay@epitech.eu>
2023-02-08 14:44:00 +0100

    sofiane.chardonnay@epitech.eu

---
+++ ex1.lua
@@ -0,0 +1,9 @@
+for oue = 1 , 100 do
+    if (oue % 3 == 0) and (oue % 5 == 0) then
+        print("ThreeFive")
+    elseif (oue % 3 == 0) then
+        print("Three")
+    elseif (oue % 5 == 0) then
+        print("Five")
+    end
+end
